11a3e43b1ec7d19eade0537fb14c63079d44e86a
Wellington <wellingtonvumu@gmail.com>
2025-06-21 06:35:13 +0200

    Update task class

--- src/Task.java
+++ src/Task.java
@@ -3,10 +3,33 @@ import java.time.LocalDateTime;
 public class Task {
     private int id;
     private String description;
+    private Status status;
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Task(String description) {
         this.description = description;
+        this.status = Status.TO_DO;
         this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void markInProgress() {
+        this.status = Status.IN_PROGRESS;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void markDone() {
+        this.status = Status.DONE;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void updateDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
     }
 }
